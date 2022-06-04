d107762dc9e50f841e651e14850eb9e23fd36a43
Maxim <mtsoy1990@gmail.com>
2022-06-05 02:53:11 +0300

    fixed Dockerfile

--- docker/redis/init.sh
+++ docker/redis/init.sh
@@ -2,9 +2,11 @@
 
 cat /redis/redis.conf
 
-#echo -e "\n\n\n" >> /usr/local/etc/redis/redis.conf
-echo "cluster-announce-ip ${CLUSTER_ANNOUNCE_IP}" >> /redis/redis.conf
-echo "cluster-announce-port ${CLUSTER_ANNOUNCE_PORT}" >> /redis/redis.conf
+echo ${CLUSTER_ANNOUNCE_IP}
+echo ${CLUSTER_ANNOUNCE_PORT}
+
+#sed 's/CLUSTER_ANNOUNCE_IP/${CLUSTER_ANNOUNCE_IP}/g' /redis/redis.conf > /redis/redis.conf
+#sed 's/CLUSTER_ANNOUNCE_PORT/${CLUSTER_ANNOUNCE_PORT}/g' /redis/redis.conf > /redis/redis.conf
 
 cat /redis/redis.conf
 

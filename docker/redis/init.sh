7f361f7f35aabba0a9816d4ad10511ef376667fd
Maxim Tsoy <mtsoy1990@gmail.com>
2022-06-05 03:59:02 +0300

    fixed Dockerfile

--- docker/redis/init.sh
+++ docker/redis/init.sh
@@ -1,13 +1,12 @@
 #!/bin/bash
 
-cat /redis/redis.conf
-
 echo ${CLUSTER_ANNOUNCE_IP}
 echo ${CLUSTER_ANNOUNCE_PORT}
 
-#sed 's/CLUSTER_ANNOUNCE_IP/${CLUSTER_ANNOUNCE_IP}/g' /redis/redis.conf > /redis/redis.conf
-#sed 's/CLUSTER_ANNOUNCE_PORT/${CLUSTER_ANNOUNCE_PORT}/g' /redis/redis.conf > /redis/redis.conf
+sed -i "s/CLUSTER_ANNOUNCE_IP/${CLUSTER_ANNOUNCE_IP}/" /redis/redis.conf
+sed -i "s/CLUSTER_ANNOUNCE_PORT/${CLUSTER_ANNOUNCE_PORT}/" /redis/redis.conf
 
 cat /redis/redis.conf
+echo " "
 
 redis-server /redis/redis.conf
